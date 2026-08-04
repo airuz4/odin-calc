d9675cdbb9db7dde1067c0d828048150960ea3ea
Airuz Balungay <balungay.airuz@gmail.com>
2026-08-04 11:54:55 +0800

    added functionality to calc
    step 0 - inputing first num
    step 1 - input operation, so that it wont be written on display
    step 2 - input 2nd num
    
    fixed functions to return the value

--- script.js
+++ script.js
@@ -3,52 +3,78 @@ function add(...parameters){
 }
 
 function subtract(...parameters){
-    return parameters.reduce((difference, item) => difference -= item,0)
+    return parameters.reduce((difference, item) => difference -= item, 0)
 }
 
 function multiply(...parameters){
-    return parameters.reduce((product, item) => product *= item,0)
+    return parameters.reduce((product, item) => product *= item,1)
 }
 
 function divide(...parameters){
-    return parameters.reduce((quotient, item) => quotient -= item,0)
+    return parameters.reduce((quotient, item) => quotient /= item, 1)
 }
 
-
-let x = 3;
-let y = 3; 
-let operand = '+';
-
-console.log(operate(x, operand, y));
 
 function operate(x, operand, y){
     if (operand == '+'){
         return add(x,y);
     }
     if (operand == '-'){
-        difference(x,y);
+        return subtract(x,y);
     }
     if (operand == '*'){
-        multiply(x,y);
+        return multiply(x,y);
     }
     if (operand == '/'){
-        divide(x,y);
+        return divide(x,y);
     }
 
 }
 
 
 const display = document.querySelector('.display');
-
+const operands = '/+*-';
 const inputButton = document.querySelector('.calc-inputs');
+let step = 0;
+let firstNum = 0;
+let operand;
+let secondNum = 0;
 
 inputButton.addEventListener('mouseup', (e) =>{
     let input = e.target.innerText;
     if (input == 'CE'){
         display.innerText = '';
+        firstNum = 0;
+        operand = '';
+        secondNum = 0;
+        step = 0;
     }
-    else {
-       display.innerText += input;
+    else if (operands.includes(input)){
+        if(step == 0){
+            firstNum = Number(display.innerText);
+            operand = input;
+            step++;
+        } 
+        else if (step == 2){
+            secondNum = Number(display.innerText);
+            display.innerText = operate(firstNum, operand, secondNum);
+            step = 0;
+        }
+        
+    }
+    else if (input == '='){
+            secondNum = Number(display.innerText);
+            display.innerText = operate(firstNum, operand, secondNum);
+            step = 0;
+    }
+    else{
+        if(step == 0 || step == 2){
+            display.innerText += input;
+        }
+        else{
+            display.innerText = input;
+            step++
+        }
     }
 
 
